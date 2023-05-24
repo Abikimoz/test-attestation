9f14d745183d8e5ff028e40cae6ea0bb18fab67f
Abikimoz <i.m.anisimov25@gmail.com>
2023-05-24 19:49:05 +0300

    step 1 done

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import _ from 'lodash'
 
 export default function solution(content){
   // BEGIN
-
+  let array = content.split('\n').slice(1);
+  array.length = array.length-1;
+  console.log('Count:', array.length);
   // END
 
 }
