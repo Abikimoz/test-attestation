417de14cd6508d1e3e475ecc74d3cf87a54bfc73
Abikimoz <i.m.anisimov25@gmail.com>
2023-05-24 23:07:03 +0300

    step 3 done

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ export default function solution(content){
   let data = content.split('\n').slice(1);
   data.length = data.length-1;
   console.log('Count:', data.length);
-  console.log(data);
+
   let cities = data.map((row) => row.split(',').at(7));
   cities = _.uniq(cities).sort().join(', ');
   console.log(`Cities: ${cities}`);
+
+  let humid = data.map((row) => row.split(',').at(3))
+
+  let humidMax = Math.max(...humid);
+  let humidMin = Math.min(...humid);
+
+  console.log(`Humidity: Min: ${humidMin}, Max: ${humidMax}`);
+
   // END
 
 }
