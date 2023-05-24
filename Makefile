904870a8c2656140c284cd2f91475630a73be54b
Abikimoz <i.m.anisimov25@gmail.com>
2023-05-24 23:18:15 +0300

    step 4 done

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ test:
 run:
 	node bin/weather.js __fixtures__/weather1.csv
 
+run2:
+	node bin/weather.js __fixtures__/weather2.csv
+
 test-steps:
 	npx jest --testNamePattern step1
 	npx jest --testNamePattern step2
--- index.js
+++ index.js
@@ -10,12 +10,21 @@ export default function solution(content){
   cities = _.uniq(cities).sort().join(', ');
   console.log(`Cities: ${cities}`);
 
-  let humid = data.map((row) => row.split(',').at(3))
+  let humid = data.map((row) => row.split(',').at(3));
 
   let humidMax = Math.max(...humid);
   let humidMin = Math.min(...humid);
 
   console.log(`Humidity: Min: ${humidMin}, Max: ${humidMax}`);
+
+  let temp = data.map((row) => row.split(',').at(1));
+  let tempMax = Math.max(...temp)
+  let tempMaxIndex = temp.indexOf(String(tempMax));
+  let HottestDay = data[tempMaxIndex];
+  let HottestDayArr = HottestDay.split(',');
+
+  console.log(`HottestDay: ${HottestDayArr[0]} ${HottestDayArr[7]}`);
+
 
   // END
 
